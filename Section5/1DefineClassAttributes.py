020c100acfb6f406b9de8db4a690bad1e1dfbad5
Shaharia-SE <shahariarony38@gmail.com>
2022-03-09 20:47:03 +0600

    classAttribute

--- Section5/1DefineClassAttributes.py
+++ Section5/1DefineClassAttributes.py
@@ -2,10 +2,10 @@ class Dog:
     # this attribute not instance
     species = "Canis Lupus"
 
-    def __init__(self, name, age, breed, color):
+    def __init__(self, name, age, breed,):
         # (below attribute instance)
         self.name = name
         self.age = age
         self.breed = breed
-        self.color = color
+
 
