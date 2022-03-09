d576c7257e4494dfa80b3f240a34b13b1992ab10
Shaharia-SE <shahariarony38@gmail.com>
2022-03-09 11:24:17 +0600

    Default Argument

---
+++ section4/DefaultArgument.py
@@ -0,0 +1,9 @@
+class Circle:
+    def __init__(self, color, radius=15):
+        self.radius = radius
+        self.color = color
+
+
+my_circle = Circle(8, "red")
+print(my_circle.radius)
+print(my_circle.color)
