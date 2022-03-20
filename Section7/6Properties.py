fa82d35f7c568cffb789c9b12dfb35947cfab76b
Shaharia-SE <shahariarony38@gmail.com>
2022-03-20 20:23:13 +0600

    property

---
+++ Section7/6Properties.py
@@ -0,0 +1,40 @@
+class Circle:
+    VALID_COLOR = ("RED", "GREEN", "BLUE")
+
+    def __init__(self, radius, color):
+        self._radius = radius
+        self._color = color
+
+    def get_radius(self):
+        return self._radius
+
+    def set_radius(self, new_radius):
+        if isinstance(new_radius, int) and new_radius > 0:
+            self._radius = new_radius
+        else:
+            print("Please enter valid radius ")
+
+    radius = property(get_radius, set_radius)
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, new_color):
+        if new_color in Circle.VALID_COLOR:
+            self._color = new_color
+        else:
+            print("Please enter valid color")
+
+    color = property(get_color, set_color)
+
+
+my_circle = Circle(10, "blue")
+# Radius
+print(my_circle.radius)
+my_circle.radius = 20
+print(my_circle.radius)
+
+# Color
+print(my_circle.color)
+my_circle.color = "RED"
+print(my_circle.color)
